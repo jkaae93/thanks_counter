a80d8734e9dc425e0617096ad3e2498d21d35433
Serim Jeon <platformbiz.jsr>
2023-12-11 11:38:03 +0900

    Add requests

---
+++ lib/constants.dart
@@ -0,0 +1,6 @@
+const String accessToken = 'ZQAAAV5of2870_dbdlRanoAZqs4o1v8s6_4F22QbkHxtqTyuJ1wsNyxSX7nsoTA9GWV944EvJLBjKwaQhsfEUEC2lpNWme9OMRplZRLv8BTkJr_3';
+const String locale = 'ko_KR';
+const String bandKey = 'AAB7EefzvWs3y9pKZl-14nEM';
+const String postKey = 'post_key 로 대체하세요.';
+const String commentKey = 'comment_key 로 대체하세요.';
+const String photoAlbumKey = 'photo_album_key 로 대체하세요.';
